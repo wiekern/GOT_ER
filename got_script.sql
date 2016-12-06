680303d8dd0a311de4bbc282410fbb494eea1479
Yafei <halloweltyan@gmail.com>
2016-12-06 13:30:59 +0100

    add tables

--- got_script.sql
+++ got_script.sql
@@ -6,7 +6,6 @@ CREATE DATABASE	got$
 CONNECT TO got$
 
 -- DROP ALL TABLES
--- db2 -x "select 'drop table ' || rtrim(tabschema) || '.' || rtrim(tabname) || ' '  || ';'from syscat.tables where type = 'T' and tabschema='DBP037'"
 drop table FIGUR$
 drop table PERSON$
 drop table ANGEHOERT$
@@ -41,17 +40,18 @@ CREATE TABLE person (
 	PRIMARY KEY (figurid)
 )$
 
-CREATE TABLE angehoert (
-	figurid INTEGER NOT NULL REFERENCES figur (figurid),
-	hausid INTEGER NOT NULL REFERENCES haus (hausid),
-	startepisode INTEGER NOT NULL,
-	endepisode INTEGER NOT NULL,
-	PRIMARY KEY (figurid, hausid)
+CREATE TABLE haus (
+	hausid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	name VARCHAR(100) NOT NULL,
+	sitz VARCHAR(50) NOT NULL,
+	motto VARCHAR(100) NOT NULL,
+	wappen VARCHAR(100) NOT NULL,
+	PRIMARY KEY (hausid)
 )$
 
 CREATE TABLE beziehung (
-	figurida INTEGER NOT NULL REFERENCES figur (figurid),
-	figuridb INTEGER NOT NULL REFERENCES figur (figurid),
+	figurida INTEGER NOT NULL REFERENCES person (figurid),
+	figuridb INTEGER NOT NULL REFERENCES person (figurid),
 	beziehungstyp VARCHAR(20) NOT NULL,
 	PRIMARY KEY (figurida, figuridb)
 )$
@@ -68,7 +68,6 @@ CREATE TABLE tier (
 	herkunftsort INTEGER NOT NULL REFERENCES ort (ortid),
 	besitztVon INTEGER,
 	PRIMARY KEY (figurid)
--- 	FOREIGN KEY (herkunftsort) REFERENCES ort (ortid)
 )$
 
 CREATE TABLE straffel (
@@ -86,15 +85,13 @@ CREATE TABLE episode (
 	Erstausstrahlungsdatum DATE NOT NULL,
 	nummer INTEGER NOT NULL REFERENCES straffel (nummer),
 	PRIMARY KEY (nummer, episodeid)
--- 	FOREIGN KEY (nummer) REFERENCES straffel (nummer)
 )$
 
 CREATE TABLE auftretenIn (
 	figurid INTEGER NOT NULL REFERENCES figur (figurid),
 	episodeid INTEGER NOT NULL,
-	nummer INTEGER NOT NULL, --straffel nummer
+	nummer INTEGER NOT NULL,
 	PRIMARY KEY (figurid, episodeid, nummer),
--- 	FOREIGN KEY (figurid) REFERENCES figur (figurid),
 	FOREIGN KEY (episodeid, nummer) REFERENCES episode (episodeid, nummer)
 )$
 
@@ -110,37 +107,25 @@ CREATE TABLE playliste (
 	playlisteid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
 	gehoertzu INTEGER REFERENCES benutzer (benutzerid),
 	PRIMARY KEY (playlisteid)
-	--FOREIGN KEY (gehoertzu) REFERENCES benutzer (benutzerid)
 )$
 
-CREATE TABLE enthalten ( -- episode playliste
+CREATE TABLE enthalten (
 	playlisteid INTEGER NOT NULL REFERENCES  playliste (playlisteid),
-	gehoertzu INTEGER NOT NULL, --benutzer id
+	gehoertzu INTEGER NOT NULL,
 	episodeid INTEGER NOT NULL,
-	nummer INTEGER NOt NULL, --straffel nummer
+	nummer INTEGER NOt NULL,
 	PRIMARY KEY (playlisteid, episodeid, nummer),
--- 	FOREIGN KEY (playlisteid) REFERENCES playliste (playlisteid),
-	-- FOREIGN KEY (gehoertzu) REFERENCES benutzer (benutzerid),
 	FOREIGN KEY (episodeid, nummer) REFERENCES episode (episodeid, nummer)
 )$
 
 
 CREATE TABLE bewertung (
-	bewertungid INTEGER NOT NULL, --PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	bewertungid INTEGER NOT NULL,
 	textInhalt VARCHAR(500),
 	rating INTEGER NOT NULL,
 	benutzerid INTEGER NOT NULL REFERENCES benutzer (benutzerid),
 	PRIMARY KEY (bewertungid, benutzerid)
--- 	FOREIGN KEY (benutzerid) REFERENCES benutzer (benutzerid)
-)$
 
-CREATE TABLE haus (
-	hausid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
-	name VARCHAR(100) NOT NULL,
-	sitz VARCHAR(50) NOT NULL,
-	motto VARCHAR(100) NOT NULL,
-	wappen VARCHAR(100) NOT NULL,
-	PRIMARY KEY (hausid)
 )$
 
 CREATE TABLE burg (
@@ -148,7 +133,6 @@ CREATE TABLE burg (
 	name VARCHAR(100) NOT NULL,
 	standort INTEGER NOT NULL REFERENCES ort (ortid),
 	PRIMARY KEY (burgid)
--- 	FOREIGN KEY (standort) REFERENCES ort (ortid)
 )$
 
 CREATE TABLE ansehen (
@@ -157,34 +141,57 @@ CREATE TABLE ansehen (
 	burgid INTEGER NOT NULL REFERENCES burg (burgid),
 	hausid INTEGER NOT NULL REFERENCES haus (hausid),
 	PRIMARY KEY (hausid, burgid)
--- 	FOREIGN KEY (hausid) REFERENCES haus (hausid),
--- 	FOREIGN KEY (burgid) REFERENCES burg (burgid),
--- 	FOREIGN KEY (standort) REFERENCES ort (ortid)
 )$
 
 CREATE TABLE beherrschen (
 	hausid INTEGER NOT NULL REFERENCES haus (hausid),
 	ortid INTEGER NOT NULL REFERENCES ort (ortid),
 	startepisode INTEGER NOT NULL,
+	startstraffel INTEGER NOT NULL,
 	endepisode INTEGER NOT NULL,
-	PRIMARY KEY (hausid, ortid, startepisode, endepisode)
--- 	FOREIGN KEY (hausid) REFERENCES haus (hausid),
--- 	FOREIGN KEY (ortid) REFERENCES ort (ortid)
+	endstraffel INTEGER NOT NULL,
+	PRIMARY KEY (hausid, ortid, startstraffel, startepisode, endstraffel, endepisode),
+	FOREIGN KEY (startepisode, startstraffel) REFERENCES episode (episodeid, nummer),
+	FOREIGN KEY (endepisode, endstraffel) REFERENCES episode (episodeid, nummer)
+)$
+
+CREATE TABLE angehoert (
+	figurid INTEGER NOT NULL REFERENCES person (figurid),
+	hausid INTEGER NOT NULL REFERENCES haus (hausid),
+	startepisode INTEGER NOT NULL,
+	startstraffel INTEGER NOT NULL,
+	endepisode INTEGER NOT NULL,
+	endstraffel INTEGER NOT NULL,
+	PRIMARY KEY (figurid, hausid, startstraffel, startepisode, endstraffel, endepisode),
+	FOREIGN KEY (startepisode, startstraffel) REFERENCES episode (episodeid, nummer),
+	FOREIGN KEY (endepisode, endstraffel) REFERENCES episode (episodeid, nummer)
+)$
+
+CREATE TABLE handlungsort (
+	episodeid INTEGER NOT NULL,
+	straffelnummr INTEGER NOT NULL,
+	ortid INTEGER NOT NULL REFERENCES ort (ortid),
+	PRIMARY KEY (episodeid, straffelnummr, ortid),
+	FOREIGN KEY (episodeid, straffelnummr) REFERENCES episode (episodeid, nummer)
 )$
 
 
 
-
+-- DROP all triggers
+DROP TRIGGER neuplayliste$
+DROP TRIGGER listezunutzer$
+DROP TRIGGER straffel$
+DROP TRIGGER episode$
 
 CREATE TRIGGER neuplayliste AFTER INSERT ON playliste
 FOR EACH STATEMENT MODE DB2SQL
-INSERT INTO straffel (episodenanzahl, startsdatum) VALUES (10, DATE('2011-04-17')) ,(10, DATE('2012-04-01')), (10, DATE('2013-03-31')), (10, DATE('2013-04-06')), (10, DATE('2015-04-12')), (10, DATE('2016-04-24'))$
-
-CREATE TRIGGER listezunutzer BEFORE INSERT ON playliste
-FOR EACH ROW 
-BEGIN
-INSERT INTO benutzer (loginkennung, name, passwort) VALUES ('user', 'user', '12345');
-END$
+INSERT INTO straffel (episodenanzahl, startsdatum) VALUES 
+(10, DATE('2011-04-17')), 
+(10, DATE('2012-04-01')), 
+(10, DATE('2013-03-31')), 
+(10, DATE('2013-04-06')), 
+(10, DATE('2015-04-12')), 
+(10, DATE('2016-04-24'))$
 
 CREATE TRIGGER straffel AFTER INSERT ON straffel
 REFERENCING NEW s
@@ -203,15 +210,70 @@ IF (s.nummer = 1) THEN
 	 (9, 'Winter Is Coming', 'fehlt', DATE('2011-06-12'), s.nummer), 
 	 (10, 'Fire and Blood', 'fehlt', DATE('2011-06-19'), s.nummer);
 ELSEIF (s.nummer = 2) THEN
-	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES (0, 'Trailer', 'Trailer', DATE('2011-04-19'), s.nummer), (1, 'The North Remembers', 'fehlt', DATE('2012-04-01'), s.nummer), (2, 'The Night Lands', 'fehlt', DATE('2012-04-08'), s.nummer), (3, 'What Is Dead May Never Die', 'fehlt', DATE('2012-04-15'), s.nummer), (4, 'Garden of Bones', 'fehlt', DATE('2012-04-22'), s.nummer), (5, 'The Ghost of Harrenhal', 'fehlt', DATE('2012-04-29'), s.nummer), (6, 'The Old Gods and the New', 'fehlt', DATE('2012-05-06'), s.nummer), (7, 'A Man Without Honor', 'fehlt', DATE('2012-05-13'), s.nummer), (8, 'The Prince of Winterfall', 'fehlt', DATE('2012-05-20'), s.nummer), (9, 'Blackwater', 'fehlt', DATE('2012-05-27'), s.nummer), (10, 'Valar Morghulis', 'fehlt', DATE('2012-06-03'), s.nummer);
+	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES 
+	(0, 'Trailer', 'Trailer', DATE('2011-04-19'), s.nummer), 
+	(1, 'The North Remembers', 'fehlt', DATE('2012-04-01'), s.nummer), 
+	(2, 'The Night Lands', 'fehlt', DATE('2012-04-08'), s.nummer), 
+	(3, 'What Is Dead May Never Die', 'fehlt', DATE('2012-04-15'), s.nummer), 
+	(4, 'Garden of Bones', 'fehlt', DATE('2012-04-22'), s.nummer), 
+	(5, 'The Ghost of Harrenhal', 'fehlt', DATE('2012-04-29'), s.nummer), 
+	(6, 'The Old Gods and the New', 'fehlt', DATE('2012-05-06'), s.nummer), 
+	(7, 'A Man Without Honor', 'fehlt', DATE('2012-05-13'), s.nummer), 
+	(8, 'The Prince of Winterfall', 'fehlt', DATE('2012-05-20'), s.nummer), 
+	(9, 'Blackwater', 'fehlt', DATE('2012-05-27'), s.nummer), 
+	(10, 'Valar Morghulis', 'fehlt', DATE('2012-06-03'), s.nummer);
 ELSEIF (s.nummer = 3) THEN
-	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES (0, 'Trailer', 'Trailer', DATE('2012-04-10'), s.nummer), (1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), (2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), (3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), (4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), (5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), (6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), (7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), (8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), (9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), (10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
+	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES 
+	(0, 'Trailer', 'Trailer', DATE('2012-04-10'), s.nummer), 
+	(1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), 
+	(2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), 
+	(3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), 
+	(4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), 
+	(5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), 
+	(6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), 
+	(7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), 
+	(8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), 
+	(9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), 
+	(10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
 ELSEIF (s.nummer = 4) THEN
-	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES (0, 'Trailer', 'Trailer', DATE('2013-04-02'), s.nummer), (1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), (2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), (3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), (4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), (5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), (6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), (7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), (8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), (9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), (10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
+	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES 
+	(0, 'Trailer', 'Trailer', DATE('2013-04-02'), s.nummer), 
+	(1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), 
+	(2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), 
+	(3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), 
+	(4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), 
+	(5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), 
+	(6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), 
+	(7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), 
+	(8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), 
+	(9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), 
+	(10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
 ELSEIF (s.nummer = 5) THEN
-	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES (0, 'Trailer', 'Trailer', DATE('2014-04-08'), s.nummer), (1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), (2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), (3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), (4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), (5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), (6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), (7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), (8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), (9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), (10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);	
+	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES 
+	(0, 'Trailer', 'Trailer', DATE('2014-04-08'), s.nummer), 
+	(1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), 
+	(2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), 
+	(3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), 
+	(4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), 
+	(5, 'Kissed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), 
+	(6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), 
+	(7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), 
+	(8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), 
+	(9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), 
+	(10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);	
 ELSEIF (s.nummer = 6) THEN
-	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES (0, 'Trailer', 'Trailer', DATE('2014-04-08'), s.nummer), (1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), (2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), (3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), (4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), (5, 'Kis, sed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), (6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), (7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), (8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), (9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), (10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
+	INSERT INTO episode (epinummer, titel, inhaltsangabe, Erstausstrahlungsdatum, nummer) VALUES 
+	(0, 'Trailer', 'Trailer', DATE('2014-04-08'), s.nummer), 
+	(1, 'Valar Dohaeris', 'fehlt', DATE('2013-03-31'), s.nummer), 
+	(2, 'Dark Wings, Dark Words', 'fehlt', DATE('2013-04-07'), s.nummer), 
+	(3, 'Walk of Punishment', 'fehlt', DATE('2013-04-14'), s.nummer), 
+	(4, 'And Now His Watch Is Ended', 'fehlt', DATE('2013-04-21'), s.nummer), 
+	(5, 'Kis, sed by Fire', 'fehlt', DATE('2013-04-28'), s.nummer), 
+	(6, 'The Climb', 'fehlt', DATE('2013-05-05'), s.nummer), 
+	(7, 'The Bear and the Maiden Fair', 'fehlt', DATE('2013-05-12'), s.nummer), 
+	(8, 'Second Sons', 'fehlt', DATE('2013-05-19'), s.nummer), 
+	(9, 'The Rains of Castamere', 'fehlt', DATE('2013-06-02'), s.nummer), 
+	(10, 'Mhysa', 'fehlt', DATE('2013-06-09'), s.nummer);
 END IF
 $
 
@@ -222,32 +284,16 @@ INSERT INTO enthalten (playlisteid, gehoertzu, episodeid, nummer)
 SELECT playlisteid, gehoertzu, episodeid, nummer FROM playliste p, new_episode e WHERE e.epinummer = 0$
 
 
---DELETE DATA from talbes
+-- DELETE DATA from talbes
 DELETE FROM benutzer$
 DELETE FROM enthalten$
 DELETE FROM episode$
 DELETE FROM straffel$
 DELETE FROM playliste$
 
+-- Test
+INSERT INTO benutzer (loginkennung, name, passwort) VALUES ('test', 'hallo', 'world')$
 INSERT INTO playliste (gehoertzu) values (1)$
 SELECT e.playlisteid, e.gehoertzu, ep.nummer, ep.titel, ep.ERSTAUSSTRAHLUNGSDATUM 
 FROM enthalten e INNER JOIN episode ep ON e.episodeid = ep.episodeid$
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
